b30eb1e8345d4aa1f005cb2a0551ca67819ac769
Andrei Robson da Silva <silva.andrei@outlook.com.br>
2024-06-22 21:20:01 +0000

    Alteracoes na classe Conta.java
    Signed-off-by: Andrei Robson da Silva <silva.andrei@outlook.com.br>

--- desafio-banco-dio/src/main/java/com/bancodigital/model/Conta.java
+++ desafio-banco-dio/src/main/java/com/bancodigital/model/Conta.java
@@ -4,5 +4,34 @@ public class Conta {
     private Banco banco;
     private Cliente cliente;
     private TipoConta tipoConta;
-    
+    private double saldo;
+
+public void sacar(){
+
+}
+
+public void depositar(){
+
+}
+
+public void tranferir(){
+
+}
+
+public Banco getBanco(){
+    return banco;
+}
+
+public Cliente getCliente(){
+    return cliente;
+}
+
+public TipoConta getTipoConta(){
+    return tipoConta;
+}
+
+public double getSaldo(){
+    return saldo;
+}
+
 }
